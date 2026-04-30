b6834690f48c88d53bc3bd3e3da14ca756f50de1
tommypagychoi <108319883+tommypagychoi@users.noreply.github.com>
2026-04-30 11:26:44 +0900

    Disable default nginx site during web deploy

--- scripts/deploy-webserver.ps1
+++ scripts/deploy-webserver.ps1
@@ -12,4 +12,4 @@ $root = Resolve-Path "$PSScriptRoot\.."
 
 & $ScpExe -i $SshKey -P $RemotePort "$root\frontend\index.html" "${User}@${RemoteHost}:/tmp/index.html"
 & $ScpExe -i $SshKey -P $RemotePort "$root\deploy\nginx-webserver.conf" "${User}@${RemoteHost}:/tmp/fde.conf"
-& $SshExe -i $SshKey -p $RemotePort "${User}@${RemoteHost}" "mkdir -p /var/www/fde && mv /tmp/index.html /var/www/fde/index.html && mv /tmp/fde.conf /etc/nginx/sites-available/fde.conf && ln -sf /etc/nginx/sites-available/fde.conf /etc/nginx/sites-enabled/fde.conf && nginx -t && systemctl reload nginx"
+& $SshExe -i $SshKey -p $RemotePort "${User}@${RemoteHost}" "mkdir -p /var/www/fde && mv /tmp/index.html /var/www/fde/index.html && mv /tmp/fde.conf /etc/nginx/sites-available/fde.conf && rm -f /etc/nginx/sites-enabled/default && ln -sf /etc/nginx/sites-available/fde.conf /etc/nginx/sites-enabled/fde.conf && nginx -t && systemctl reload nginx"
