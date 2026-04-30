b9a9cc27133161280d220bac420e3ac7941560b9
tommypagychoi <108319883+tommypagychoi@users.noreply.github.com>
2026-04-30 10:25:06 +0900

    Finalize root webserver deploy script

--- scripts/deploy-webserver.ps1
+++ scripts/deploy-webserver.ps1
@@ -1,13 +1,15 @@
 param(
     [string]$RemoteHost = "ccymproxmox.iptime.org",
-    [string]$User = "tommypagy",
+    [string]$User = "root",
     [int]$RemotePort = 2086,
-    [string]$SshKey = "$PSScriptRoot\..\secrets\fde_deploy_key"
+    [string]$SshKey = "$PSScriptRoot\..\secrets\fde_deploy_key",
+    [string]$SshExe = "C:\Windows\System32\OpenSSH\ssh.exe",
+    [string]$ScpExe = "C:\Windows\System32\OpenSSH\scp.exe"
 )
 
 $ErrorActionPreference = "Stop"
 $root = Resolve-Path "$PSScriptRoot\.."
 
-scp -i $SshKey -P $RemotePort "$root\frontend\index.html" "${User}@${RemoteHost}:/tmp/index.html"
-scp -i $SshKey -P $RemotePort "$root\deploy\nginx-webserver.conf" "${User}@${RemoteHost}:/tmp/fde.conf"
-ssh -i $SshKey -p $RemotePort "${User}@${RemoteHost}" "sudo mkdir -p /var/www/fde && sudo mv /tmp/index.html /var/www/fde/index.html && sudo mv /tmp/fde.conf /etc/nginx/sites-available/fde.conf && sudo ln -sf /etc/nginx/sites-available/fde.conf /etc/nginx/sites-enabled/fde.conf && sudo nginx -t && sudo systemctl reload nginx"
+& $ScpExe -i $SshKey -P $RemotePort "$root\frontend\index.html" "${User}@${RemoteHost}:/tmp/index.html"
+& $ScpExe -i $SshKey -P $RemotePort "$root\deploy\nginx-webserver.conf" "${User}@${RemoteHost}:/tmp/fde.conf"
+& $SshExe -i $SshKey -p $RemotePort "${User}@${RemoteHost}" "mkdir -p /var/www/fde && mv /tmp/index.html /var/www/fde/index.html && mv /tmp/fde.conf /etc/nginx/sites-available/fde.conf && ln -sf /etc/nginx/sites-available/fde.conf /etc/nginx/sites-enabled/fde.conf && nginx -t && systemctl reload nginx"
