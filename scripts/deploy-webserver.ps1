886b27b20174f77e7215ab07512e813f6e4c1670
tommypagychoi <108319883+tommypagychoi@users.noreply.github.com>
2026-04-30 10:03:35 +0900

    Use remote SSH port for webserver deploy

--- scripts/deploy-webserver.ps1
+++ scripts/deploy-webserver.ps1
@@ -1,12 +1,13 @@
 param(
-    [string]$WebServer = "192.168.0.86",
-    [string]$User = "ubuntu",
+    [string]$RemoteHost = "ccymproxmox.iptime.org",
+    [string]$User = "tommypagy",
+    [int]$RemotePort = 2086,
     [string]$SshKey = "$env:USERPROFILE\.ssh\id_rsa"
 )
 
 $ErrorActionPreference = "Stop"
 $root = Resolve-Path "$PSScriptRoot\.."
 
-scp -i $SshKey "$root\frontend\index.html" "${User}@${WebServer}:/tmp/index.html"
-scp -i $SshKey "$root\deploy\nginx-webserver.conf" "${User}@${WebServer}:/tmp/fde.conf"
-ssh -i $SshKey "${User}@${WebServer}" "sudo mkdir -p /var/www/fde && sudo mv /tmp/index.html /var/www/fde/index.html && sudo mv /tmp/fde.conf /etc/nginx/sites-available/fde.conf && sudo ln -sf /etc/nginx/sites-available/fde.conf /etc/nginx/sites-enabled/fde.conf && sudo nginx -t && sudo systemctl reload nginx"
+scp -i $SshKey -P $RemotePort "$root\frontend\index.html" "${User}@${RemoteHost}:/tmp/index.html"
+scp -i $SshKey -P $RemotePort "$root\deploy\nginx-webserver.conf" "${User}@${RemoteHost}:/tmp/fde.conf"
+ssh -i $SshKey -p $RemotePort "${User}@${RemoteHost}" "sudo mkdir -p /var/www/fde && sudo mv /tmp/index.html /var/www/fde/index.html && sudo mv /tmp/fde.conf /etc/nginx/sites-available/fde.conf && sudo ln -sf /etc/nginx/sites-available/fde.conf /etc/nginx/sites-enabled/fde.conf && sudo nginx -t && sudo systemctl reload nginx"
